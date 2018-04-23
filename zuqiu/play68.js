89f4d5679993dfde2c3d4307e73029a7e93cc579
小沫 <xiaomo94@foxmail.com>
2018-04-23 12:14:52 +0800

    提交

---
+++ zuqiu/play68.js
@@ -0,0 +1,21 @@
+﻿
+function play68_init() {
+	updateShare(0);
+}
+
+function goHome() {
+	window.location.href = HOME_PATH;
+}
+
+function play68_submitScore(score) {
+	updateShareScore(score);
+	setTimeout( function() { show_share(); }, 1500 )
+}
+
+function updateShare(bestScore) {
+
+}
+
+function updateShareScore(bestScore) {
+
+}
